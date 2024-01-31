b46e1445d4c77dd7313fe2266fa5fd984e47b6a5
Macarena Parra <parrameca@gmail.com>
2024-01-30 23:05:11 -0400

    Url change

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     clientId: '52cd2946efdd498088a6e1d6248e048d',
     clientSecret: 'ccfd3d275730499ab0b49906f8ceb174',
     access_token:'',
-    redirectUri:'http://localhost:4200?'
+    redirectUri:'https://defontana-test-ten.vercel.app'
   };
